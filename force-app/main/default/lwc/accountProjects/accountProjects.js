fe43f079b4bee30a38a3c08bc6f18fb5c9b113e3
Lautaro Casas <lautarocasas12@gmail.com>
2025-06-12 13:09:20 -0300

    Modificaciones para manejar correctamente una cuenta seleccionada

--- force-app/main/default/lwc/accountProjects/accountProjects.js
+++ force-app/main/default/lwc/accountProjects/accountProjects.js
@@ -2,31 +2,73 @@ import { LightningElement, api, wire } from 'lwc';
 import getProjectsByAccount from '@salesforce/apex/ProjectController.getProjectsByAccount';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { refreshApex } from '@salesforce/apex';
+import { deleteRecord } from 'lightning/uiRecordApi';
 
 import NAME_FIELD from '@salesforce/schema/Proyecto__c.Name';
 import PRESUPUESTO_FIELD from '@salesforce/schema/Proyecto__c.Presupuesto__c';
 import ESTADO_FIELD from '@salesforce/schema/Proyecto__c.Estado__c';
 import CONTACTO_FIELD from '@salesforce/schema/Proyecto__c.Contact__c';
 
-export default class AccountProjects extends LightningElement {
-    @api accountId;
+const actions = [
+    { label: 'Show details', name: 'show_details' },
+    { label: 'Delete', name: 'delete' },
+];
 
-    @wire(getProjectsByAccount, { accountId: '$accountId' })
+const columns = [
+    { label: 'Nombre', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Contacto relacionado', fieldName: CONTACTO_FIELD.fieldApiName, type: 'text' },
+    { label: 'Presupuesto', fieldName: PRESUPUESTO_FIELD.fieldApiName, type: 'currency' },
+    { label: 'Estado', fieldName: ESTADO_FIELD.fieldApiName, type: 'text' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions }
+    }
+];
+
+export default class AccountProjects extends LightningElement {
+    @api account;
+    columns = columns;
+
+    @wire(getProjectsByAccount, { accountId: '$account.Id' })
     projects;
 
     get projects() {
         return this.projects?.data;
     }
 
-    columns = [
-        { label: 'Nombre', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
-        { label: 'Contacto relacionado', fieldName: CONTACTO_FIELD.fieldApiName, type: 'text' },
-        { label: 'Presupuesto', fieldName: PRESUPUESTO_FIELD.fieldApiName, type: 'currency' },
-        { label: 'Estado', fieldName: ESTADO_FIELD.fieldApiName, type: 'text' }
-    ];
+    get accountId(){
+        return this.account?.Id;
+    }
 
-    // Campos para el formulario
-    fields = [NAME_FIELD, CONTACTO_FIELD, PRESUPUESTO_FIELD, ESTADO_FIELD];
+    get cardTitle() {
+        return `Proyectos asociados a la cuenta ${this.account?.Name || ''}`;
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+
+        if (actionName === 'delete') {
+            deleteRecord(row.Id).
+            then(()=>{
+                const toastEvent = new ShowToastEvent({
+                title: "Registro borrado",
+                message: "Record ID: " + event.detail.id,
+                variant: "success"
+                });
+                this.dispatchEvent(toastEvent);
+                refreshApex(this.projects);
+            })
+            .catch(error => {
+                const toastEvent = new ShowToastEvent({
+                    title: 'Error al eliminar',
+                    message: error.body.message,
+                    variant: 'error'
+                });
+                this.dispatchEvent(toastEvent);
+            });
+        }
+    }
 
     handleSuccess(event) {
         const toastEvent = new ShowToastEvent({
